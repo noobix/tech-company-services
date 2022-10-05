7dcf9a1b12c472689b6628a06973c75145533b13
noobix <nanayaw@live.com>
2022-10-05 04:17:25 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let fullname=document.querySelector(".name")
+let email=document.querySelector(".email")
+let phone=document.querySelector(".phone")
+let cartegory=document.querySelector(".cartegory")
+let description=document.querySelector(".description")
+
+// name.addEventListener("input",()=>{
+//     console.log(name.value)
+// })
+// let btnSubmit=document.querySelector(".button")
+// console.log(btnSubmit);
+// btnSubmit.addEventListener("click",()=>{
+//     console.log(fullname.value,email.value,phone.value,cartegory.value)
+// })
+
+function getdata(){
+    console.log(fullname.value,email.value,phone.value,cartegory.value,description.value)
+}
